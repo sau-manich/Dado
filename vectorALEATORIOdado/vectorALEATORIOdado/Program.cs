703c797a1b294b85e57965bdf91675af1437c999
saul-manich <saulmamani73@gmail.com>
2016-08-12 22:20:45 -0400

    Mi primer random dado

---
+++ vectorALEATORIOdado/vectorALEATORIOdado/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectorALEATORIOdado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dado = new Random();
+            int resultado = 0;
+            resultado = dado.Next(1, 100);
+            Console.WriteLine("el numero ganador es {0}",resultado);
+            Console.ReadKey();
+        }
+    }
+}
